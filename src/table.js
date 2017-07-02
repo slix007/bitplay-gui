3905ddfa5caa6945d3459c0d2f621b8eceb33987
Sergei Shurmin <slixsl007@gmail.com>
2017-07-02 22:51:29 +0200

    okCoin type of placing orders

--- src/table.js
+++ src/table.js
@@ -201,6 +201,10 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
         }
         isStopMoving.innerHTML = isEnabled;
     };
+    let repaintPlacingType = function (returnData) {
+        let placingType = document.getElementById("okcoin-placing-type");
+        placingType.innerHTML = returnData.secondMarket;
+    };
     let repaintStates = function (returnData) {
         let elementById = document.getElementById("markets-states");
         elementById.innerHTML = 'Market is ready for new signals(flag isBusy and openOrders.size==0). '
@@ -348,6 +352,9 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
         // markets order is opposite for deltas
         fetch('/market/stop-moving', function (returnData) {
             repaintStopMoving(returnData);
+        });
+        fetch('/market/placing-type', function (returnData) {
+            repaintPlacingType(returnData);
         });
         fetch('/market/states', function (returnData) {
             repaintStates(returnData);
@@ -946,6 +953,26 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
                     requestData,
                     function (responseData, resultElement) {
                         repaintStates(JSON.parse(responseData));
+                    },
+                    null
+                );
+            }
+
+            if (element.id == 'update-okcoin-placing-type') {
+                let placingType = document.getElementById("okcoin-placing-type").innerHTML;
+                if (placingType == 'maker') {
+                    placingType = 'taker';
+                } else {
+                    placingType = 'maker';
+                }
+                let request = {firstMarket: 'maker', secondMarket: placingType};
+                let requestData = JSON.stringify(request);
+                console.log(requestData);
+
+                httpAsyncPost(baseUrl + '/market/placing-type',
+                    requestData,
+                    function (responseData, resultElement) {
+                        repaintPlacingType(JSON.parse(responseData));
                     },
                     null
                 );
