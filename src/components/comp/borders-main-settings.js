eca388da394008b292983379df01e3ebf7f4ee13
Sergei Shurmin <slixsl007@gmail.com>
2018-04-22 00:11:09 +0400

    avg-delta-calc first params

--- src/components/comp/borders-main-settings.js
+++ src/components/comp/borders-main-settings.js
@@ -3,40 +3,25 @@ let $ = require('jquery');
 let Http = require('../../http');
 let bordersUtils = require('./borders-utils');
 
-// let exports = module.exports = {};
-
-const firstId = 'first-state';
-const secondId = 'second-state';
-
 export function repaint(borderData, BORDERS_SETTINGS_URL) {
     let container = $('#borders-main-settings');
 
     if ($(container).children().length === 0) {
-        createVerDropdown(container, borderData.activeVersion,
-                BORDERS_SETTINGS_URL);
+        createVerDropdown(container, borderData.activeVersion, BORDERS_SETTINGS_URL);
         createPeriodSec(container, borderData, BORDERS_SETTINGS_URL);
 
-        // $('<span>').text('Bitmex state: ').appendTo(markets);
-        // $('<span>', {id: firstId}).text(returnData.firstMarket).appendTo(
-        //         markets);
-        // $('<span>').text((returnData.firstTimeToReset.length === 0 ? '' : ('('
-        //         + returnData.firstTimeToReset + 'sec)'))).appendTo(markets);
-        // $('<span>').text(', Okex state: ').appendTo(markets);
-        // $('<span>', {id: secondId}).text(returnData.secondMarket).appendTo(
-        //         markets);
-        // $('<span>').text((returnData.secondTimeToReset.length === 0 ? '' : ('('
-        //         + returnData.secondTimeToReset + 'sec)'))).appendTo(markets);
-    }
+        createDeltaCalcTypeDropdown(container, borderData, BORDERS_SETTINGS_URL);
+        createDeltaCalcPast(container, borderData, BORDERS_SETTINGS_URL);
 
-    //
-    // updateState(firstId, returnData.firstMarket);
-    // updateState(secondId, returnData.secondMarket);
+        createDeltaSaveType(container, borderData, BORDERS_SETTINGS_URL);
+        deltaSaveDev(container, borderData, BORDERS_SETTINGS_URL);
+        deltaSavePerSec(container, borderData, BORDERS_SETTINGS_URL);
+    }
 }
 
 function createVerDropdown(parent, ver, BORDERS_SETTINGS_URL) {
     let container = $('<span>',{id:'border-select-version'}).appendTo(parent);
-    // let container = document.getElementById("border-select-version");
-    let label = $('<span>').text('Borders version').appendTo(container);
+    $('<span>').text('Borders version').appendTo(container);
 
     let select = document.createElement('select');
     let option1 = document.createElement('option');
@@ -58,7 +43,6 @@ function createVerDropdown(parent, ver, BORDERS_SETTINGS_URL) {
 
 function createPeriodSec(parent, borderData, BORDERS_SETTINGS_URL) {
     let container = $('<div>').appendTo(parent);
-    // let container = document.cre("border-period-sec");
     let label = document.createElement('span');
     label.innerHTML = 'Recalc period sec';
     container.append(label);
@@ -80,4 +64,119 @@ function createPeriodSec(parent, borderData, BORDERS_SETTINGS_URL) {
     container.append(resultLabel);
 }
 
+function createDeltaCalcTypeDropdown(parent, borderData, BORDERS_SETTINGS_URL) {
+    let container = $('<span>').appendTo(parent);
+    $('<span>').text('DeltaCalcType: ').appendTo(container);
 
+    let select = $('<select>');
+    select.append($('<option>').val('DELTA').text('DELTA'));
+    select.append($('<option>').val('AVG_DELTA').text('AVG_DELTA'));
+    select.change(function () {
+        select.disabled = true;
+        const requestData = JSON.stringify({borderDelta: {deltaCalcType: this.value}});
+        console.log(requestData);
+
+        Http.httpAsyncPost(BORDERS_SETTINGS_URL,
+                requestData, function (result) {
+                    alert('Result' + result);
+                    select.disabled = false;
+                });
+    });
+    select.val(borderData.borderDelta.deltaCalcType);
+
+    container.append(select);
+}
+
+function createDeltaCalcPast(parent, borderData, BORDERS_SETTINGS_URL) {
+    let container = $('<div>').appendTo(parent);
+    $('<span>').text('deltaCalcPast').appendTo(container);
+    let edit = $('<input>').width('80px').appendTo(container);
+    let resultLabel = $('<span>').text(borderData.borderDelta.deltaCalcPast);
+    let setBtn = $('<button>').text('set').click(function () {
+        setBtn.disabled = true;
+
+        const requestData = JSON.stringify({borderDelta: {deltaCalcPast: edit.val()}});
+        console.log(requestData);
+
+        Http.httpAsyncPost(BORDERS_SETTINGS_URL,
+                requestData, function (result) {
+                    const res = JSON.parse(result);
+                    resultLabel.text(res.result);
+                    setBtn.disabled = false;
+                });
+    });
+
+    container.append(setBtn);
+    container.append(resultLabel);
+}
+
+function createDeltaSaveType(parent, borderData, BORDERS_SETTINGS_URL) {
+    let container = $('<span>').appendTo(parent);
+    $('<span>').text('DeltaSaveType: ').appendTo(container);
+
+    let select = $('<select>');
+    select.append($('<option>').val('DEVIATION').text('DEVIATION'));
+    select.append($('<option>').val('PERIODIC').text('PERIODIC'));
+    select.append($('<option>').val('ALL').text('ALL'));
+    select.change(function () {
+        select.disabled = true;
+        const requestData = JSON.stringify({borderDelta: {deltaSaveType: this.value}});
+        console.log(requestData);
+
+        Http.httpAsyncPost(BORDERS_SETTINGS_URL,
+                requestData, function (result) {
+                    alert('Result' + result);
+                    select.disabled = false;
+                });
+    });
+    select.val(borderData.borderDelta.deltaSaveType);
+
+    container.append(select);
+}
+
+function deltaSaveDev(parent, borderData, BORDERS_SETTINGS_URL) {
+    let container = $('<div>').appendTo(parent);
+    $('<span>').text('deltaSaveDev').appendTo(container);
+    let edit = $('<input>').width('80px').appendTo(container);
+    let resultLabel = $('<span>').text(borderData.borderDelta.deltaSaveDev);
+    let setBtn = $('<button>').text('set').click(function () {
+        setBtn.disabled = true;
+
+        const requestData = JSON.stringify({borderDelta: {deltaSaveDev: edit.val()}});
+        console.log(requestData);
+
+        Http.httpAsyncPost(BORDERS_SETTINGS_URL,
+                requestData, function (result) {
+                    const res = JSON.parse(result);
+                    resultLabel.text(res.result);
+                    setBtn.disabled = false;
+                });
+    });
+
+    container.append(setBtn);
+    container.append(resultLabel);
+}
+
+
+function deltaSavePerSec(parent, borderData, BORDERS_SETTINGS_URL) {
+    let container = $('<div>').appendTo(parent);
+    $('<span>').text('deltaSavePerSec').appendTo(container);
+    let edit = $('<input>').width('80px').appendTo(container);
+    let resultLabel = $('<span>').text(borderData.borderDelta.deltaSavePerSec);
+    let setBtn = $('<button>').text('set').click(function () {
+        setBtn.disabled = true;
+
+        const requestData = JSON.stringify({borderDelta: {deltaSavePerSec: edit.val()}});
+        console.log(requestData);
+
+        Http.httpAsyncPost(BORDERS_SETTINGS_URL,
+                requestData, function (result) {
+                    const res = JSON.parse(result);
+                    resultLabel.text(res.result);
+                    setBtn.disabled = false;
+                });
+    });
+
+    container.append(setBtn);
+    container.append(resultLabel);
+}
