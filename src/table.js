3e4ea367c56efed1f853610dee5f72271ee05388
SergeiShurmin <slixsl007@gmail.com>
2018-07-18 14:22:31 +0400

    remove old comments

--- src/table.js
+++ src/table.js
@@ -139,8 +139,6 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
         let deltaSmaUpdateIn = document.getElementById("deltaSmaUpdateIn");
         let border1 = document.getElementById("border1");
         let border2 = document.getElementById("border2");
-        // let makerDelta = document.getElementById("maker-delta");
-        // let bu = document.getElementById("bu");
         let cumDelta = document.getElementById("cum-delta");
         let cumDeltaFact = document.getElementById("cum-delta-fact");
         let cumDiffFactBr = document.getElementById("cum-diff-fact-br");
@@ -169,8 +167,6 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
                 : 'Border_sma update in ' + returnData.deltaSmaUpdateIn + ' sec.';
         border1.innerHTML = returnData.border1;
         border2.innerHTML = returnData.border2;
-        // makerDelta.innerHTML = returnData.makerDelta;
-        // bu.innerHTML = returnData.buValue;
         cumDelta.innerHTML = sprintf('%s/%s', returnData.cumDelta, returnData.cumAstDelta);
         cumDeltaFact.innerHTML = sprintf('%s/%s', returnData.cumDeltaFact, returnData.cumAstDeltaFact);
         cumDiffFactBr.innerHTML = sprintf('%s', returnData.cumDiffFactBr);
@@ -682,36 +678,6 @@ exports.onDomLoadedFunc = function (firstMarketName, secondMarketName, baseUrl) 
                     null
                 );
             }
-            //
-            // if (element.id == 'update-maker-delta') {
-            //     let newMakerDeltaValue = document.getElementById('maker-delta-edit').value;
-            //     let request = {makerDelta: newMakerDeltaValue};
-            //     let requestData = JSON.stringify(request);
-            //     console.log(requestData);
-            //
-            //     httpAsyncPost(baseUrl + '/market/update-maker-delta',
-            //         requestData,
-            //         function (responseData, resultElement) {
-            //             repaintDeltasAndBorders(responseData);
-            //         },
-            //         null
-            //     );
-            // }
-
-            // if (element.id == 'update-bu') {
-            //     let newPeriodSecValue = document.getElementById('bu-edit').value;
-            //     let request = {buValue: newPeriodSecValue};
-            //     let requestData = JSON.stringify(request);
-            //     console.log(requestData);
-            //
-            //     httpAsyncPost(baseUrl + '/market/update-maker-delta',
-            //         requestData,
-            //         function (responseData, resultElement) {
-            //             repaintDeltasAndBorders(responseData);
-            //         },
-            //         null
-            //     );
-            // }
 
             if (element.id == 'update-cum-delta') {
                 let newCumDeltaValue = document.getElementById('cum-delta-edit').value;
