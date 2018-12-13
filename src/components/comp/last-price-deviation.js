fdba61e42290149fa54689a20ca37fc3a3206f16
Sergei Shurmin <slixsl007@gmail.com>
2018-12-13 13:56:55 +0400

    night-alarm-fix LPD timer

--- src/components/comp/last-price-deviation.js
+++ src/components/comp/last-price-deviation.js
@@ -7,6 +7,7 @@ var exports = module.exports = {};
 
 // const resLabel = $('<span>');
 const resLabel = $('<span>');
+const lbDelaySec = $('<span>');
 const lbBitmex = $('<span>');
 const lbOkex = $('<span>');
 
@@ -15,46 +16,74 @@ exports.fillComponents = function (jsonData, baseUrl) {
 
     const indexCont = $('#last-price-deviation');
     if (indexCont.children().length === 0) {
-        let edit = $('<input>').width('80px');
-        let setBtn = $('<button>').text('set');
-        setBtn.click(function () {
-            setBtn.disabled = true;
-            let requestObj = {maxDevUsd: edit.val()};
-            const requestData = JSON.stringify(requestObj);
-            console.log(requestData);
-
-            Http.httpAsyncPost(SET_URL, requestData, function (rawRes) {
-                const res = JSON.parse(rawRes);
-                updateInfo(res);
-                setBtn.disabled = false;
-            });
-        });
-        let fixCurrBtn = $('<button>').text('fix current');
-        fixCurrBtn.click(function () {
-            fixCurrBtn.disabled = true;
-            let requestObj = {};
-            const requestData = JSON.stringify(requestObj);
-            console.log(requestData);
-
-            let FIX_URL = baseUrl + '/market/last-price-deviation/fix';
-            Http.httpAsyncPost(FIX_URL, requestData, function (rawRes) {
-                const res = JSON.parse(rawRes);
-                updateInfo(res);
-                fixCurrBtn.disabled = false;
-            });
-        });
-
-        indexCont.append($('<span>').html('LastPrice deviation'));
-        indexCont.append(edit);
-        indexCont.append(setBtn);
-        indexCont.append(resLabel);
-        indexCont.append(fixCurrBtn);
-        indexCont.append(lbBitmex);
-        indexCont.append(lbOkex);
+        addLPD(SET_URL, baseUrl, indexCont);
+        indexCont.append($('<br>'));
+        addLPDDelay(SET_URL, baseUrl, indexCont);
     }
 
     updateInfo(jsonData);
 };
+
+function addLPD(SET_URL, baseUrl, indexCont) {
+    let edit = $('<input>').width('80px');
+    let setBtn = $('<button>').text('set');
+    setBtn.click(function () {
+        setBtn.disabled = true;
+        let requestObj = {maxDevUsd: edit.val()};
+        const requestData = JSON.stringify(requestObj);
+        console.log(requestData);
+
+        Http.httpAsyncPost(SET_URL, requestData, function (rawRes) {
+            const res = JSON.parse(rawRes);
+            updateInfo(res);
+            setBtn.disabled = false;
+        });
+    });
+    let fixCurrBtn = $('<button>').text('fix current');
+    fixCurrBtn.click(function () {
+        fixCurrBtn.disabled = true;
+        let requestObj = {};
+        const requestData = JSON.stringify(requestObj);
+        console.log(requestData);
+
+        let FIX_URL = baseUrl + '/market/last-price-deviation/fix';
+        Http.httpAsyncPost(FIX_URL, requestData, function (rawRes) {
+            const res = JSON.parse(rawRes);
+            updateInfo(res);
+            fixCurrBtn.disabled = false;
+        });
+    });
+
+    indexCont.append($('<span>').prop('title', 'last price deviation').html('LPD: '));
+    indexCont.append(edit);
+    indexCont.append(setBtn);
+    indexCont.append(resLabel);
+    indexCont.append(fixCurrBtn);
+    indexCont.append(lbBitmex);
+    indexCont.append(lbOkex);
+}
+
+function addLPDDelay(SET_URL, baseUrl, indexCont) {
+    let edit = $('<input>').width('80px');
+    let setBtn = $('<button>').text('set');
+    setBtn.click(function () {
+        setBtn.disabled = true;
+        let requestObj = {delaySec: edit.val()};
+        const requestData = JSON.stringify(requestObj);
+        console.log(requestData);
+
+        Http.httpAsyncPost(SET_URL, requestData, function (rawRes) {
+            const res = JSON.parse(rawRes);
+            updateInfo(res);
+            setBtn.disabled = false;
+        });
+    });
+
+    indexCont.append($('<span>').prop('title', 'last price deviation period in seconds').html('LPD period: '));
+    indexCont.append(edit);
+    indexCont.append(setBtn);
+    indexCont.append(lbDelaySec);
+}
 
 const getRoundVal = x => ((x.toString().includes('.')) ? (x.toString().split('.').pop().length) : (0));
 
@@ -78,4 +107,6 @@ function updateInfo(jsonData) {
     resLabel.html(sprintf('%s usd  ', jsonData.maxDevUsd));
     showDev(lbBitmex, jsonData.bitmexMainExceed, 'bitmex=', jsonData.bitmexMain, jsonData.bitmexMainCurr);
     showDev(lbOkex, jsonData.okexMainExceed, ', okex=', jsonData.okexMain, jsonData.okexMainCurr);
+
+    lbDelaySec.html(sprintf('%s sec. To fixCurrent(sec): %s', jsonData.delaySec, jsonData.toNextFix));
 }
