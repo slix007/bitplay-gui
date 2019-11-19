6b2aa6b81f9cfac85326b2182c6d21dd74e3a0fb
Sergei Shurmin <slixsl007@gmail.com>
2019-11-19 12:01:42 +0400

    recovery-nt-usd looks ok

--- src/components/recovery-nt-usd.js
+++ src/components/recovery-nt-usd.js
@@ -1,62 +1,70 @@
-'use strict';
+'use strict'
 
-import Http from "../http";
-import $ from "jquery";
-import * as mobx from "mobx";
-import {mobxStore} from "../store/settings-store";
+import Http from '../http'
+import $ from 'jquery'
+import { allSettings, setAllSettingsRaw } from '../store/settings-store'
 
-export {createRecoveryNtUsd};
+export { createRecoveryNtUsd }
 
 let createRecoveryNtUsd = function (baseUrl) {
 
-    recovery(baseUrl);
+  recovery(baseUrl)
 
-};
+}
 
-function recovery(baseUrl) {
-    const URL_ACTION = baseUrl + '/market/recovery-nt-usd';
+function recovery (baseUrl) {
+  const URL_ACTION = baseUrl + '/market/recovery-nt-usd'
 
-    const cont = $('#recovery-nt-usd');
-    const checkbox = $('<input>').css('margin-left', '10px').attr('type', 'checkbox').appendTo(cont);
-    const lbInfo = $('<span>').text('enable ').appendTo(cont);
-    const btn = $('<button>').text('recovery nt_usd').appendTo(cont);
-    const lb = $('<span>').appendTo(cont);
+  const cont = $('#recovery-nt-usd')
+  const checkbox = $('<input>').attr('type', 'checkbox').appendTo(cont)
+  const btn = $('<button>').text('recovery nt_usd').appendTo(cont)
+  const lb = $('<span>').appendTo(cont)
 
-    lbInfo.css('color', 'grey');
-    btn.prop('disabled', true);
+  btn.prop('disabled', true)
 
-    btn.click(() => {
-        // let confirmation = window.confirm("Bitmex: close all positions\n\nAre you sure?");
-        // if (confirmation)
-        {
-            // console.log('request to ' + URL_ACTION);
-            btn.prop('disabled', true);
-            lb.text('in progress');
+  btn.click(() => {
+    // let confirmation = window.confirm("Bitmex: close all positions\n\nAre you sure?");
+    // if (confirmation)
+    {
+      // console.log('request to ' + URL_ACTION);
+      btn.prop('disabled', true)
+      lb.text('in progress')
 
-            Http.httpAsyncPost(URL_ACTION,
-                    "", function (rawData) {
-                        const result = JSON.parse(rawData);
-                  lb.text(rawData);
-                        // if (result.orderId && result.orderId.length > 0) {
-                        //     lb.text('orderId: ' + result.orderId + ' ' + result.details);
-                        // } else {
-                        //     lb.text('Error: ' + result.details);
-                        // }
-                        btn.prop('disabled', false);
-                    });
-        }
-    });
+      Http.httpAsyncPost(URL_ACTION,
+        '', function (rawData) {
+          const result = JSON.parse(rawData)
+          lb.text(result.result)
+          // if (result.orderId && result.orderId.length > 0) {
+          //     lb.text('orderId: ' + result.orderId + ' ' + result.details);
+          // } else {
+          //     lb.text('Error: ' + result.details);
+          // }
+          btn.prop('disabled', false)
+        })
+    }
+  })
 
-    checkbox.click(function () {
-        if (checkbox.prop('checked')) {
-            lbInfo.css('color', 'black');
-            btn.prop('disabled', false);
-            btn.addClass('redBtn');
-        } else {
-            lbInfo.css('color', 'grey');
-            btn.prop('disabled', true);
-            btn.removeClass('redBtn');
-        }
-    });
+  function setManualMode () {
+    checkbox.prop('disabled', true)
+    const data = JSON.stringify({ manageType: 'MANUAL' })
+    Http.httpAsyncPost(allSettings.SETTINGS_URL, data,
+      json => {
+        checkbox.prop('disabled', false)
+        const res = setAllSettingsRaw(json)
+        // alert('New value: ' + res.manageType);
+      })
+  }
+
+  checkbox.click(() => {
+    if (checkbox.prop('checked')) {
+      btn.prop('disabled', false)
+      btn.addClass('redBtn')
+      setManualMode()
+    } else {
+      btn.prop('disabled', true)
+      btn.removeClass('redBtn')
+    }
+
+  })
 }
 
